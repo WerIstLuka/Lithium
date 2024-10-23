256470bc3f0a1cb62873e4a546ecd46440872cbc
luka <weristluka@gmail.com>
2024-10-23 17:52:23 +0200

    made instruction list more readable

--- Main.py
+++ Main.py
@@ -7,25 +7,25 @@ from lib.CheckInput import CheckInput
 
 #cpu arch
 #name, num, arguments, info
-Instructions = []
-Instructions.append(["mov", 1, 2, "none"])
-Instructions.append(["add", 2, 2, "none"])
-Instructions.append(["sub", 3, 2, "none"])
-Instructions.append(["inc", 4, 1, "none"])
-Instructions.append(["dec", 5, 1, "none"])
-Instructions.append(["jmp", 6, 1, "jump"])
-Instructions.append(["jov", 7, 1, "jump"])
-Instructions.append(["jnz", 8, 1, "jump"])
-Instructions.append(["jiz", 9, 1, "jump"])
-Instructions.append(["ld", 10, 2, "none"])
-Instructions.append(["st", 11, 2, "back"])
-Instructions.append(["and", 12, 2, "none"])
-Instructions.append(["xor", 13, 2, "none"])
-Instructions.append(["not", 14, 1, "none"])
-Instructions.append(["or", 15, 2, "none"])
-Instructions.append(["shl", 16, 1, "none"])
-Instructions.append(["shr", 17, 1, "none"])
-Instructions.append(["slp", 18, 1, "none"])
+Instructions = [
+	["mov", 1, 2, "none"],
+	["add", 2, 2, "none"],
+	["sub", 3, 2, "none"],
+	["inc", 4, 1, "none"],
+	["dec", 5, 1, "none"],
+	["jmp", 6, 1, "jump"],
+	["jov", 7, 1, "jump"],
+	["jnz", 8, 1, "jump"],
+	["jiz", 9, 1, "jump"],
+	["ld", 10, 2, "none"],
+	["st", 11, 2, "back"],
+	["and", 12, 2, "none"],
+	["xor", 13, 2, "none"],
+	["not", 14, 1, "none"],
+	["or", 15, 2, "none"],
+	["shl", 16, 1, "none"],
+	["shr", 17, 1, "none"],
+	["slp", 18, 1, "none"]]
 
 File = ReadFile("Source.li")
 print(File)
