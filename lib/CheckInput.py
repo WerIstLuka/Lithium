9816d57d4a59e81ae4632bd826808c9080d473f0
WerIstLuka <WerIstLuka@gmail.com>
2024-10-25 10:07:14 +0200

    added more checks to CheckInput

--- lib/CheckInput.py
+++ lib/CheckInput.py
@@ -8,12 +8,13 @@ def CheckInput(Input, Instructions):
 	DefinedFunctions = []
 	for Line in Input:
 		for Word in Line:
-			IsInstruction = False
-			for Instruction in Instructions:
-				if Word == Instruction[0]:
-					IsInstruction = True
-					break
-			if IsInstruction == False:
+			if Word[0] != "$" and IsInt(Word[1:]) == False:
 				if IsInt(Word) == False:
-					print(Word)
+					IsInstruction = False
+					for Instruction in Instructions:
+						if Word == Instruction[0]:
+							IsInstruction = True
+							break
+					if IsInstruction == False:
+						print(Word)
 		LineCounter += 1
