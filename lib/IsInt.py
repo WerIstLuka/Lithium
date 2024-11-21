b9799a02027854df627d67c604f76444c2d77d57
luka <weristluka@gmail.com>
2024-11-21 11:51:12 +0100

    improved IsInt

--- lib/IsInt.py
+++ lib/IsInt.py
@@ -3,25 +3,18 @@
 def IsInt(Num):
 	try:
 		int(Num)
-	except Exception:
-		if len(Num) > 2:
-			IsBin = False
-			IsHex = False
-			if Num[:2] == "0x":
-				try:
-					int(Num, 16)
-					IsHex = True
-				except Exception:
-					return "NaN"
-			if Num[:2] == "0b":
-				try:
-					int(Num, 2)
-					IsBin = True
-				except Exception:
-					return "NaN"
-			if IsHex == True:
-				return int(Num, 16)
-			if IsBin == True:
+		return int(Num)
+	except ValueError:
+		if Num[:2] == "0b":
+			try:
+				int(Num, 2)
 				return int(Num, 2)
-		return "NaN"
-	return int(Num)
+			except ValueError:
+				pass
+		if Num[:2] == "0x":
+			try:
+				int(Num, 16)
+				return int(Num, 16)
+			except ValueError:
+				pass
+	return "NaN"
