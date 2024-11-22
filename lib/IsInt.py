a390efff1e7268ec0cfab9151b5d69c063a9533a
WerIstLuka <weristluka@gmail.com>
2024-11-22 08:51:44 +0000

    improved IsInt readability

--- lib/IsInt.py
+++ lib/IsInt.py
@@ -5,16 +5,17 @@ def IsInt(Num):
 		int(Num)
 		return int(Num)
 	except ValueError:
-		if Num[:2] == "0b":
-			try:
-				int(Num, 2)
-				return int(Num, 2)
-			except ValueError:
-				pass
-		if Num[:2] == "0x":
-			try:
-				int(Num, 16)
-				return int(Num, 16)
-			except ValueError:
-				pass
+		pass
+	if Num[:2] == "0b":
+		try:
+			int(Num, 2)
+			return int(Num, 2)
+		except ValueError:
+			pass
+	if Num[:2] == "0x":
+		try:
+			int(Num, 16)
+			return int(Num, 16)
+		except ValueError:
+			pass
 	return "NaN"
